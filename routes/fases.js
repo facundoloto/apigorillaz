17dce1ed5c64849d6ce03fa40c769c5ba60adbc8
Facundo_Loto <55114127+facundoloto@users.noreply.github.com>
2021-11-24 00:56:38 -0300

    Update fases.js

--- routes/fases.js
+++ routes/fases.js
@@ -6,7 +6,7 @@ const query=util.promisify(conexion.query).bind(conexion)//configuramos utiil pa
 
 async function fases(req,res){
 try{
-let sql='SELECT fases.fase,fases.historia,discos.nombre_disco,villanos.villano FROM fases JOIN discos ON fases.id_disk = discos.id_disk JOIN villanos ON fases.villano=villanos.id ORDER BY fase'
+let sql='SELECT fases.fase,fases.historia,fases.url_imagen,discos.nombre_disco,villanos.villano FROM fases JOIN discos ON fases.id_disk = discos.id_disk JOIN villanos ON fases.villano=villanos.id ORDER BY fase'
 const rows = await query(sql) //la query configurada con util se pueden usar con async/await para cargar muchos datos y hacer soncrionas las consultas en js
 let sqlNew=`select fase,historia,discos.nombre_disco from fases JOIN discos ON fases.id_disk=discos.id_disk where fase='fase 4' `
 const rowsNew = await query(sqlNew)
